8ebf0d6e6b3533201ea951c193b466b57a25960f
gholib <gtrandnet@gmail.com>
2021-10-21 12:49:54 +0300

    russification

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ TOKEN = '2031350902:AAGC-gDYOnBchwCfySCSj7ac7_XHZEEqW3A'
 const bot = new TelegramBot(TOKEN,{polling:true})
 bot.on('message', msg=>{
     console.log("ggg");
-    bot.sendMessage(msg.chat.id, `hello from Heroku, ${msg.from.first_name}`)
+    bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}`)
 })
